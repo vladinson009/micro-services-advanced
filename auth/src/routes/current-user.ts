2ba28d13f679c56edbc6074793319e95a03bbf7b
Vladimir Gulev <vladinson009@icloud.com>
2026-07-04 22:14:22 +0200

    auth service

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -2,6 +2,10 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', () => {});
+router.get('/api/users/currentuser', (req, res) => {
+    console.log('currentUser ');
+
+    res.send('Hi there!');
+});
 
 export { router as currentUserRouter };
