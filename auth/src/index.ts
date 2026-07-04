3e88af25622924ff6f585a84476e084f4dcc3f80
Vladimir Gulev <vladinson009@icloud.com>
2026-07-04 18:10:39 +0200

    init

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 
+import { currentUserRouter } from './routes/current-user.js';
+
 const app = express();
 
 app.use(express.json());
 
-app.get('/api/users/currentuser', (req, res) => {
-    res.send('Hi There!!!');
-});
+app.use(currentUserRouter);
 
 app.listen(3000, () => {
     console.log('Listening on port 3000!!!!!');
