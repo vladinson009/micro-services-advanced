0a26b4e612d9dfb3492045d04dd5121f015edcad
Vladimir Gulev <vladinson009@icloud.com>
2026-07-05 17:57:09 +0200

    User schema

---
+++ auth/src/models/user.ts
@@ -0,0 +1,30 @@
+import mongoose, { InferSchemaType } from 'mongoose';
+
+// An interface that describes the properties
+// That are required to create a new User
+interface UserAttrs {
+    email: string;
+    password: string;
+}
+// An interface that describes the properties
+// that a User Model has
+interface UserModel extends mongoose.Model<UserDoc> {
+    build(attrs: UserAttrs): UserDoc;
+}
+
+// An interface that describes the properties
+// that a User Document has
+interface UserDoc extends mongoose.Document {
+    email: string;
+    password: string;
+}
+
+const userSchema = new mongoose.Schema({
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+});
+userSchema.statics.build = (attrs: UserAttrs) => new User(attrs);
+
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
+
+export { User };
