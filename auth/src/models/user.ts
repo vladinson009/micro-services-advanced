a4cec69328dcd75e28b49644c6079d4f69953809
Vladimir Gulev <vladinson009@icloud.com>
2026-07-05 18:28:22 +0200

    Password hash

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,4 +1,5 @@
-import mongoose, { InferSchemaType } from 'mongoose';
+import mongoose from 'mongoose';
+import { Password } from '../services/password';
 
 // An interface that describes the properties
 // That are required to create a new User
@@ -23,6 +24,14 @@ const userSchema = new mongoose.Schema({
     email: { type: String, required: true },
     password: { type: String, required: true },
 });
+
+userSchema.pre('save', async function () {
+    if (this.isModified('password')) {
+        const hashed = await Password.toHash(this.get('password'));
+        this.set('password', hashed);
+    }
+});
+
 userSchema.statics.build = (attrs: UserAttrs) => new User(attrs);
 
 const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
