4f8081a9668d6c74521a71a9679c7bed34471e58
Vladimir Gulev <vladinson009@icloud.com>
2026-07-07 22:55:36 +0200

    jwt & env & session

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
 import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
@@ -8,9 +9,18 @@ import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
 import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
+
 const app = express();
 
+app.set('trust proxy', true);
+
 app.use(express.json());
+app.use(
+    cookieSession({
+        signed: false, // Disable encryption
+        secure: true,
+    }),
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
@@ -23,6 +33,9 @@ app.all('*', async (req, res, next) => {
 app.use(errorHandler);
 
 const start = async () => {
+    if (!process.env.JWT_KEY) {
+        throw new Error('No JWT_KEY env');
+    }
     try {
         await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
         console.log('Connected to MongoDB...');
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
 import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validation-error';
 import { BadRequestError } from '../errors/bad-request-error';
@@ -34,6 +36,21 @@ router.post(
             password,
         });
         await user.save();
+
+        // Generate JWT
+
+        const userJwt = jwt.sign(
+            {
+                id: user._id,
+                email: user.email,
+            },
+            process.env.JWT_KEY!,
+        );
+
+        // Store it on session object
+        req.session = {
+            jwt: userJwt,
+        };
 
         res.status(201).send(user);
     },
