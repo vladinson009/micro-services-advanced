5e313649d6587470230e0badf3482e464beb0d16
Vladimir Gulev <vladinson009@icloud.com>
2026-08-21 22:27:15 +0200

    orders service

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,24 @@
+import mongoose from 'mongoose';
 import express, { Request, type Response } from 'express';
+import { requireAuth, validateRequest } from '@tickets-vg/common';
+import { body } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/orders', async (req: Request, res: Response) => {
-  res.send({});
-});
+router.post(
+  '/api/orders',
+  requireAuth,
+  [
+    body('ticketId')
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage('ticketId must be provided'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    res.send({});
+  },
+);
 
 export { router as newOrderRouter };
