25b66a61e6eb80ed458c5b582a2e3f9dfb029846
Vladimir Gulev <vladinson009@icloud.com>
2026-08-22 17:47:17 +0200

    custom method

--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -11,11 +11,13 @@ export interface TicketDoc extends mongoose.Document {
   title: string;
   price: number;
   id: string;
+  version: number;
   isReserved(): Promise<boolean>;
 }
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
   build(attrs: TicketAttrs): TicketDoc;
+  findByEvent(event: { id: string; version: number }): Promise<TicketDoc | null>;
 }
 
 const ticketSchema = new mongoose.Schema(
@@ -48,6 +50,24 @@ ticketSchema.statics.build = (attrs: TicketAttrs) => {
     _id: attrs.id,
     title: attrs.title,
     price: attrs.price,
+  });
+};
+
+//! Must be commented. Its just an example
+/*
+ Manually optimistic Concurrency 
+ ticketSchema.pre('save', function (done) {
+   this.$where = {
+     version: this.get('version') - 1,
+   };
+   done;
+ }); 
+ */
+
+ticketSchema.statics.findByEvent = (event: { id: string; version: number }) => {
+  return Ticket.findOne({
+    _id: event.id,
+    version: event.version - 1,
   });
 };
 ticketSchema.methods.isReserved = async function () {
