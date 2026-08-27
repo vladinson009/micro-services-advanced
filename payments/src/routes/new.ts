8a62121b123f82e38df75cf7fbb85ebff1f0eda8
Vladimir Gulev <vladinson009@icloud.com>
2026-08-27 22:17:51 +0200

    bug: fixed bug with currency integer

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -41,7 +41,7 @@ router.post(
 
     const charge = await stripe.charges.create({
       currency: 'usd',
-      amount: order.price * 100,
+      amount: Math.round(order.price * 100),
       source: token,
     });
 
